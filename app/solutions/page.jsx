d68ead77621a6163282181938983fbc0032dc59d
ALI JAN <133298273+AliJanGithub@users.noreply.github.com>
2025-12-01 12:19:40 +0500

    solutions page developed

--- app/solutions/page.jsx
+++ app/solutions/page.jsx
@@ -1,8 +1,19 @@
+import TrustedPartnersSlider from '../../components/TrustedPartnersSlider'
 import Navigation from '../../components/Navigation'
 import React from 'react'
+import ServicesSection from '../../components/ServicesSection'
+import WhyUsSection from '../../components/WhyUsSection'
+import Footer from '../../components/Footer'
 
 export default function page() {
   return (
+    <>
+   
     <Navigation/>
+     <TrustedPartnersSlider/>
+      <ServicesSection />
+        <WhyUsSection />
+        <Footer/>
+      </>
   )
 }
